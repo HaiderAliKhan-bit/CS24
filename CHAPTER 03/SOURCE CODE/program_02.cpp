5675e56730f9767f6cbb132fb82cdafedac45245
HaiderAliKhan-bit <aghahaiderali.2007@gmail.com>
2024-10-24 10:58:04 -0700

    Add files via upload

---
+++ CHAPTER 03/SOURCE CODE/program_02.cpp
@@ -0,0 +1,22 @@
+// TODO: INTAKE LENGHT AND BREADTH OF A RECTANGLE AND PRINTING OUT THE AREA
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // NOTE: DECLARATION OF VARIABLES:
+    float l, w, area;
+
+    // NOTE: GETTING INPUT/ INITIALIZATION OF VARIABLES
+    cout << "ENTER THE LENGHT OF RECTANGLE: ";
+    cin >> l;
+    cout << "ENTER THE WIDTH OF RECTANGLE: ";
+    cin >> w;
+    // NOTE: INITIALIZATION OF VARIABLES
+    area = l * w;
+
+    // NOTE: PRINTING THE RESULT:
+    cout << "THE AREA OF RECTANGLE IS: " << area << endl;
+    return 0;
+}
